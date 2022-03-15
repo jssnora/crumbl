a1c9a0f650ed17f8ff17b8e3842c324b7822ecf5
jssnora <js.s@live.com>
2022-03-15 22:17:22 +1100

    adds method to verify post request is from Stripe webhook

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,6 +6,21 @@ class PaymentsController < ApplicationController
     end
 
     def webhook
+        #creates event to verify post request is from Stripe
+        begin
+            payload = request.raw_post
+            header = request.headers['HTTP_STRIPE_SIGNATURE']
+            secret = Rails.application.credentials.dig(:stripe, :webhook_signing_secret)
+            event = Stripe::Webhook.construct_event(payload, header, secret)
+        #raise exceptions to catch errors from webhook construct event
+        rescue Stripe::SignatureVerificationError => e
+            render json: {error: "Unauthorised"}, status: 403
+            return
+        rescue JSON::ParserError => e
+            render json: {error: "Bad request"}, status: 422
+            return
+        end
+
         payment_intent_id = params[:data][:object][:payment_intent]
         payment = Stripe::PaymentIntent.retrieve(payment_intent_id)
         listing_id = payment.metadata.listing_id 
