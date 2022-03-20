61af8cd9dc73f01de47ff363a487e8d91453cee9
jssnora <js.s@live.com>
2022-03-20 11:54:49 +1100

    fixes bug so a buyer can only see their own order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_order, only: [:show]
-  before_action :authorise_user, only: [:show]
 
   def index
     @orders = Order.all
@@ -12,12 +11,6 @@ class OrdersController < ApplicationController
   end
 
   private
-
-  def authorise_user
-    if @order.buyer_id != current_user.id
-      redirect_to listings_path, alert: "You don't have permission to do that"
-    end
-  end
 
   def set_order
     @order = Order.find(params[:id])
